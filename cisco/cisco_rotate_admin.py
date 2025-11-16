7b82565daf03b105e680a996424476b578201c81
ByteNetx <taoyuca@gmail.com>
2025-11-16 09:21:54 -0800

    Update cisco_rotate_admin.py

--- cisco/cisco_rotate_admin.py
+++ cisco/cisco_rotate_admin.py
@@ -1,5 +1,5 @@
 import argparse, getpass, datetime, re, os
-import yaml, jinja2
+import yaml, jinja2, sys
 from cryptography.fernet import Fernet
 from pathlib import Path
 from netmiko import ConnectHandler
@@ -51,27 +51,23 @@ def runner():
     else:
         username = os.getlogin()
 
-    basePath = Path.home() / 'pyenv3.9' / 'cisco'
+    basePath = Path.home() / 'netdev' / 'python-env' / 'cisco'
     devFile = f"{basePath}/data/{args.f}"
 
     with open(devFile, 'r') as f:
         data = yaml.safe_load(f)
 
-    devices = data['devices']
+    devData = data['devices']
     cfgData = data['cfg-data']
-    credFile = f"{basePath}/data/{data['credFile']}"
+    devices = {devType: devList for devType, devList in devData.items() if devList is not None}
+    if any(Value is None for Key, Value in cfgData.items()):
+        err = f"Missing the required configuration parameters in the following device file!\n{devFile}"
+        sys.exit(err)
     
+    credFile = f"{basePath}/data/{data['credFile']}"
+
     with open(credFile, "rb") as f:
         encrypted_credentials= f.read()
-
-    datenow = datetime.datetime.now().strftime("%Y%m%d")
-    logFile = f"{basePath}/logs/change_logs_{datenow}.txt"
-    logwriter = open(logFile, 'a')
-
-    templatePath = f"{basePath}/templates"
-    loader = jinja2.FileSystemLoader(searchpath=templatePath)
-    env = jinja2.Environment(autoescape=False, loader=loader)
-    cisco_template = env.get_template(cfgData['template'])
     
     myKey = input("Enter decryption key:")
     f  = Fernet(myKey)
@@ -81,49 +77,59 @@ def runner():
         'new_enable': credentials.split()[1].strip()
     })
     
+    templatePath = f"{basePath}/templates"
+    loader = jinja2.FileSystemLoader(searchpath=templatePath)
+    env = jinja2.Environment(autoescape=False, loader=loader)
+    cisco_template = env.get_template(cfgData['template'])
+
+    datenow = datetime.datetime.now().strftime("%Y%m%d")
+    logFile = f"{basePath}/logs/change_logs_{datenow}.txt"
+    logwriter = open(logFile, 'a')
+
     for devType, devList in devices.items():
         for each in devList:
-            dev = DEVICE(each, username, args.password, args.password, devType)
-            device = {
-                'host': dev.host,
-                'username': dev.username,
-                'password': dev.password,
-                'secret': dev.secret,
-                'device_type': dev.device_type
-            }
-            if connect(device,logwriter):
-                current_prompt = ssh_connect.find_prompt()
-                devName = current_prompt.strip("#")
-                print(f"\nConnected to {device['host']}-{devName} to deploy configurations")
-                timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-                logwriter.write(f"+++++++++++++++ {timestamp}-{device['host']}-{devName} +++++++++++++++\n")
-                cis_cfg = cisco_template.render(cfgData)
+            cfg_data = {}
+            if each is not None:
+                dev = DEVICE(each, username, args.password, args.password, devType)
+                device = {
+                    'host': dev.host,
+                    'username': dev.username,
+                    'password': dev.password,
+                    'secret': dev.secret,
+                    'device_type': dev.device_type
+                }
+                cfg_data.update({'dev_type': devType})
+                cfg_data.update(cfgData)
+                cis_cfg = cisco_template.render(cfg_data)
                 cis_cfg = "".join([s for s in cis_cfg.splitlines(True) if (not re.search(r"^\s*$", s))])
-                deploy_cfg = ssh_connect.send_config_set(cis_cfg.split('\n'))
+                if connect(device,logwriter):
+                    current_prompt = ssh_connect.find_prompt()
+                    devName = current_prompt.strip("#")
+                    print(f"\nConnected to {device['host']}-{devName} to deploy configurations")
+                    timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+                    logwriter.write(f"+++++++++++++++ {timestamp}-{device['host']}-{devName} +++++++++++++++\n")
+                    
+                    deploy_cfg = ssh_connect.send_config_set(cis_cfg.split('\n'))
+        
+                    showUser = ssh_connect.send_command('show run | i username')
+                    output = re.findall(r"username\s(\S+)\s",showUser)
+                    if output:
+                        for user in cfg_data['current_admin']:
+                            if user in output:
+                                deploy_cfg += ssh_connect.send_multiline_timing(
+                                    ["conf term",f"no username {user}","\n","end"]
+                                )
     
-                showUser = ssh_connect.send_command('show run | i username')
-                output = re.findall(r"username\s(\S+)\s",showUser)
-                if output:
-                    for user in cfgData['current_admin']:
-                        if user in output:
-                            deploy_cfg += ssh_connect.send_multiline_timing(
-                                ["conf term",f"no username {user}","\n","end"]
-                            )
-
-                if devType == "cisco_ios":
-                    save_cfg = ssh_connect.save_config()
-                elif devType == "cisco_nxos":
-                    save_cfg = ssh_connect.send_multiline_timing(
-                        ["copy running-config startup-config","\n"]
-                    )
-                logwriter.write(f"{showUser}\n\n{deploy_cfg}\n\n{save_cfg}\n\n")
-                ssh_connect.disconnect()
+                    if devType == "cisco_ios":
+                        save_cfg = ssh_connect.save_config()
+                    elif devType == "cisco_nxos":
+                        save_cfg = ssh_connect.send_multiline_timing(
+                            ["copy running-config startup-config","\n"]
+                        )
+                    logwriter.write(f"{showUser}\n\n{deploy_cfg}\n\n{save_cfg}\n\n")
+                    ssh_connect.disconnect()
     
     logwriter.close()
     print('Task completed!')
 if __name__ == "__main__":
     runner()
-
-
-
-
