c533888bd469591b803e67feb7d644d9873efc73
ByteNetx <taoyuca@gmail.com>
2025-11-21 04:08:13 -0800

    Update password_hash.py

--- cisco/password_hash.py
+++ cisco/password_hash.py
@@ -15,17 +15,24 @@ class InvalidPassword(Exception):
     """
     pass
 
-def banner(Type):
+def banner():
     print(f"""
-############################################################
-# Converts a plain text password into a Cisco Type{Type} secret #
-############################################################
+################################################################
+# Cisco Secret Type 8&9 Password Hasher. The password must be  #
+#  - At least 16 characters long                                #
+#  - Contains at least one uppercase letter                    #
+#  - Contains at least one lowercase letter                    #
+#  - Contains at least one digit                               #
+#  - Contains at least one special character                   #
+#  - Does not contain three consecutive identical characters   #
+################################################################
 """)
 
 def validate_password(pwd):
     invalid_chars = r"(?=.+?[/?:,.\'\\])"
-    password_pattern = r"^(?=.+?[A-Z])(?=.+?[a-z])(?=.+?[0-9])(?=.+?[!@#$%^&*()_+=\[\]\{\};\"<>|]).{16,16}$"
-    if not re.fullmatch(password_pattern, pwd) or re.search(r"\s+", pwd) or re.search(invalid_chars, pwd):
+    password_pattern = r"^(?!.*(\w)\1{2,})(?!.*(\s))(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=\[\]\{\};\"<>|]).{16,20}$"
+    if not re.match(password_pattern, pwd):
+    #or re.search(r"\s+", pwd) or re.search(invalid_chars, pwd):
         raise InvalidPassword(r"""
 Password does not meet the policy policy:
  - Password must be at least 16 characters long
@@ -36,32 +43,34 @@ Password does not meet the policy policy:
      !@#$%^&*()_+\[\]\{\};\"<>|
 """)
 
-def hash_type8():
-    banner(8)
-    while True:
-        pwd = input(Fore.GREEN+"Enter a plain text password:"+Fore.RESET)
-        try:
-            validate_password(pwd)
-        except InvalidPassword as exception_string:
-            print(exception_string)
-            pass
-        except KeyboardInterrupt:
-            sys.exit()
-        else:
-            salt_chars = []
-            for _ in range(14):
-                salt_chars.append(random.choice(cisco_b64chars))
-            salt = "".join(salt_chars)
-            # Create the hash
-            hash = hashlib.pbkdf2_hmac('sha256', pwd.encode('utf-8'), salt.encode(), 20000, 32)
-            # Convert the hash from Standard Base64 to Cisco Base64
-            hash = base64.b64encode(hash).decode().translate(b64table)[:-1]
-            # Print the hash in the Cisco IOS CLI format
-            pwd_type8 = f'$8${salt}${hash}'
-            return pwd_type8
+def hash_type8(pwd):
+    salt_chars = []
+    for _ in range(14):
+        salt_chars.append(random.choice(cisco_b64chars))
+    salt = "".join(salt_chars)
+    # Create the hash
+    hash = hashlib.pbkdf2_hmac('sha256', pwd.encode('utf-8'), salt.encode(), 20000, 32)
+    # Convert the hash from Standard Base64 to Cisco Base64
+    hash = base64.b64encode(hash).decode().translate(b64table)[:-1]
+    # Print the hash in the Cisco IOS CLI format
+    pwd_type8 = f'$8${salt}${hash}'
+    return pwd_type8
 
-def hash_type9():
-    banner(9)
+def hash_type9(pwd):
+    salt_chars = []
+    for _ in range(14):
+        salt_chars.append(random.choice(cisco_b64chars))
+    salt = "".join(salt_chars)
+    # Create the hash
+    hash = hashlib.scrypt(pwd.encode('utf-8'), salt=salt.encode(), n=16384, r=1, p=1, dklen=32)
+    # Convert the hash from Standard Base64 to Cisco Base64
+    hash = base64.b64encode(hash).decode().translate(b64table)[:-1]
+    # Print the hash in the Cisco IOS CLI format
+    pwd_type9 = f'$9${salt}${hash}'
+    return pwd_type9
+
+def main():
+    banner()
     while True:
         pwd = input(Fore.GREEN+"Enter a plain text password:"+Fore.RESET)
         try:
@@ -72,14 +81,22 @@ def hash_type9():
         except KeyboardInterrupt:
             sys.exit()
         else:
-            salt_chars = []
-            for _ in range(14):
-                salt_chars.append(random.choice(cisco_b64chars))
-            salt = "".join(salt_chars)
-            # Create the hash
-            hash = hashlib.scrypt(pwd.encode('utf-8'), salt=salt.encode(), n=16384, r=1, p=1, dklen=32)
-            # Convert the hash from Standard Base64 to Cisco Base64
-            hash = base64.b64encode(hash).decode().translate(b64table)[:-1]
-            # Print the hash in the Cisco IOS CLI format
-            pwd_type9 = f'$9${salt}${hash}'
-            return pwd_type9
+            if len(sys.argv) > 1:
+                if sys.argv[1] == '8':
+                    secret  = hash_type8(pwd)
+                    print(f"Cisco type8 secret:{Fore.BLUE}{secret}")
+                elif sys.argv[1] == '9':
+                    secret = hash_type9(pwd)
+                    print(f"Cisco type9 secret:{Fore.BLUE}{secret}")
+                validate = input("Enter \"yes\" to continue or \"no\" to quit:")
+                if validate != "y" and validate != "yes":
+                    sys.exit()
+            else:
+                secret = hash_type9(pwd)
+                print(f"Cisco type9 secret:{Fore.BLUE}{secret}")
+                validate = input("Enter \"yes\" to continue or \"no\" to quit:")
+                if validate != "y" and validate != "yes":
+                    sys.exit()
+
+if __name__ == "__main__":
+    main()
