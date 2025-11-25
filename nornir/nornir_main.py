e9f53a212b67ecc7207fefb275cb988a8cf5a755
ByteNetx <taoyuca@gmail.com>
2025-11-25 12:55:51 -0800

    Update nornir_main.py

--- nornir/nornir_main.py
+++ nornir/nornir_main.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
+import argparse, getpass, os, json
 from nornir import InitNornir
 from nornir_utils.plugins.functions import print_result
 from nornir_napalm.plugins.tasks import napalm_get, napalm_cli, napalm_configure
 from nornir.core.task import Task
 import pandas as pd
-import json
+from pathlib import Path
 from xlsxwriter.color import Color
-
-nr = InitNornir(
-    config_file="config.yaml"
-)
 
 def run_tasks(task: Task):
     #task.run(
@@ -24,29 +21,57 @@ def run_tasks(task: Task):
     #    replace=False
     #)
 
-try:
-    results = nr.run(
-        task=run_tasks
+def main():
+    class PASSWORD(argparse.Action):
+        def __call__(self, parser, namespace, values, option_string):
+            if values is None:
+                values = getpass.getpass()
+                setattr(namespace, self.dest, values)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--u', help='Username', type=str,
     )
-except Exception as e:
-    print(f"An error occurred: {e}")
-else:
-    data = []
-    try:
-        for host in results.keys():
-            data.append({
-                'hostname': results[host][1].result['facts']['hostname'],
-                'os_version': results[host][1].result['facts']['os_version'],
-                'model': results[host][1].result['facts']['model'],
-                'serial_number': results[host][1].result['facts']['serial_number']
-            })
-            print(json.dumps(data, indent=2))
-        df = pd.DataFrame(data)
-    except ValueError as e:
-        print(e)
+    parser.add_argument(
+        '--p', help='Password', action=PASSWORD, nargs='?', dest='password', type=str, required=True
+    )
+    args = parser.parse_args()
+
+    if args.u:
+        username = args.u
     else:
-        excel_file = "napalm_facts.xlsx"
-        writer = pd.ExcelWriter(excel_file, engine="xlsxwriter")
+        username = os.getlogin()
+
+    basePath = Path.home() / 'nornir-env' / 'project'
+    repFile = f"{basePath}/reports/nr_get_facts.xlsx"
+
+    nr = InitNornir(
+        config_file="config.yaml"
+    )
+
+    #nr = nr.filter()
+    nr.inventory.defaults.username = username
+    nr.inventory.defaults.password = args.password
+
+    try:
+        results = nr.run(task=run_tasks)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    else:
+        data = []
+        for host in results.keys():
+            try:
+                data.append({
+                    'hostname': results[host][1].result['facts']['hostname'],
+                    'os_version': results[host][1].result['facts']['os_version'],
+                    'model': results[host][1].result['facts']['model'],
+                    'serial_number': results[host][1].result['facts']['serial_number']
+                })
+            except ValueError as e:
+                print(e)
+    
+        df = pd.DataFrame(data)
+        writer = pd.ExcelWriter(repFile, engine="xlsxwriter")
         df.to_excel(writer, sheet_name="Inventory_UK", startrow=1, header=False)
         workbook = writer.book
         worksheet = writer.sheets["Inventory_UK"]
@@ -63,3 +88,6 @@ else:
             worksheet.write(0, col_num + 1, value, header_format)
             worksheet.autofit()
         writer.close()
+
+if __name__ == "__main__":
+    main()
