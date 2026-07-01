0b516d5f6982b6c8978f4f77fa359e7882f8f3da
ByteNetx <taoyuca@gmail.com>
2026-07-01 14:11:30 -0700

    Update pano_manager.py

--- panos/pano_object/pano_manager.py
+++ panos/pano_object/pano_manager.py
@@ -29,8 +29,9 @@ logger = logging.getLogger(__name__)
 
 class PanoramaObjectManager:
     """
-    A class to manage custom URL categories, address objects, and address groups
-    on a Panorama device group.
+    A class to manage custom URL category, address, address group,
+    service, service group, and external dynamic list objects
+    on a Panorama.
     """
 
     def __init__(self, hostname: str, username: str = None, password: str = None,
@@ -545,15 +546,28 @@ class PanoramaObjectManager:
                 Example:
                 {
                     "address_objects": [
-                        {"name": "web-server", "ip_address": "192.168.1.10"},
-                        {"name": "db-server", "ip_address": "192.168.1.20"}
+                        {"name": "web-server1", "ip_address": "192.168.1.10"},
+                        {"name": "web-server2", "ip_address": "192.168.1.20"}
                     ],
                     "address_groups": [
-                        {"name": "web-servers", "member_names": ["web-server", "web-server2"]}
+                        {"name": "web-servers", "member_names": ["web-server1", "web-server2"]}
                     ],
                     "url_categories": [
                         {"name": "Dev-Sites", "url_list": ["*.dev.local"]}
-                    ]
+                    ],
+                    "service_objects": [
+                        {"name": "tcp-3389", "protocol": "tcp", "destination_port": "3389"}
+                    ],
+                    "service_groups": [
+                        {"name": "grp-dev-service1", "value": ["tcp-3389"]}
+                    ],
+                    "edls": [
+                        {
+                            "name": "edl-blocklist-ips",
+                            "edl_type": "ip",
+                            "source": "https://example.com/PADBL/edl_blocklist_ips.txt",
+                            "repeat": "hourly"
+                        }
                 }
 
         Returns:
