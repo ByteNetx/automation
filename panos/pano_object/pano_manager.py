e18c330d33538a0b58c3553fdd7487099c213d90
ByteNetx <taoyuca@gmail.com>
2026-07-01 12:16:32 -0700

    Update pano_manager.py

--- panos/pano_object/pano_manager.py
+++ panos/pano_object/pano_manager.py
@@ -850,8 +850,8 @@ def main():
     """
 
     args = parse_arguments()
-    basepath = Path.home() / 'pyenv3.13' / 'panos'
-    filepath = f"{basepath}/config_data/{args.file}"
+    basepath = Path.home() / 'pyenv3.13' / 'panos' / 'pano_object'
+    filepath = f"{basepath}/config/{args.file}"
 
     PANORAMA_HOST = args.hostname
     API_KEY = args.apikey
